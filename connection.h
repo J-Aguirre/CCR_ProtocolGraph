da44e734956457fe10b8d72b4ac13cc559a67b5c
jorge <jgarciae12@gmail.com>
2017-11-28 13:00:42 -0500

    class connections

--- connection.h
+++ connection.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 #include <string>
+#include <string.h>
 
 using namespace std;
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
@@ -17,21 +18,33 @@ class Connection{
   public:
 
     Connection();
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
     void insert_node();
     void update_node();
     void delete_node();
     void find_node();
 
-    ~Connection(){}
+    ~Connection(){
+      sqlite3_close(db);
+    }
 };
 
 Connection::Connection(){
      /* Open database */
-     sqlite3 *db;
-     char *zErrMsg = 0;
-     int rc;
-     const char *sql;
-     rc = sqlite3_open("asdasdasdasd.db", &db);
-     cout << rc;
+     rc = sqlite3_open("protocol_db.db", &db);
+     if(rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+     } else {
+        fprintf(stderr, "Opened database successfully\n");
+     }
 
+}
+
+void Connection::insert_node(){
+  sql = "INSERT INTO nodes (name) "  \
+        "VALUES ('Peru');";
+  /* Execute SQL statement */
+  rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
 }
--- main.cpp
+++ main.cpp
@@ -5,19 +5,7 @@
 int main(int argc, char const *argv[])
 {
 
-  sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
-
-    rc = sqlite3_open("pasdadad.db", &db);
-    cout<< rc;
-    if( rc == 0 ) {
-       fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-       return(0);
-    } else {
-       fprintf(stderr, "Opened database successfully\n");
-    }
-    sqlite3_close(db);
-
+  Connection connect;
+  connect.insert_node();
 	return 0;
 }
